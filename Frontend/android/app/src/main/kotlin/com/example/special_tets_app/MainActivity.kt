4db9e506076af25fae1ff9a82ae37b05539aa9a4
ruhamags <91523337+ruhamags@users.noreply.github.com>
2022-05-21 23:45:21 +0300

    integrated backend with frontend

---
+++ Frontend/android/app/src/main/kotlin/com/example/special_tets_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.special_tets_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/example/special_tets_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.special_tets_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
