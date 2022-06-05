3815625b3a51101aaed171bdd1e4d2010f4c51bc
Hiwot Derese <Hiwotderese16@gmail.com>
2022-06-05 20:47:06 +0300

    main_file

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,115 +1,133 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:special_tests/application/hospital%20profile/bloc/hospitalBloc.dart';
+import 'package:special_tests/application/hospital_list/bloc/popularHospitalBloc.dart';
+import 'package:special_tests/presentation/Common/Color.dart';
+import 'package:special_tests/presentation/pages/index.dart';
+import 'package:special_tests/infrastructure/index.dart';
+import 'package:special_tests/application/index.dart';
+import 'package:special_tests/presentation/pages/signup/clientRegistration.dart';
+import 'package:special_tests/presentation/pages/signup/hospitalRegistration.dart';
+import 'package:special_tests/presentation/pages/special_tests/detail.dart';
+import 'package:special_tests/presentation/pages/user/CommonUserProfileEdit.dart';
+part 'routes.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+  runApp(MultiRepositoryProvider(
+    providers: [
+      RepositoryProvider(
+          create: (context) =>
+              AuthRepository(authDataProvider: AuthDataProvider())),
+      RepositoryProvider(
+          create: (context) =>
+              UserRepository(dataProvider: UserDataProvider())),
+      RepositoryProvider(
+          create: (context) =>
+              ClientRepository(dataProvider: ClientDataProvider())),
+      RepositoryProvider(
+          create: (context) =>
+              HospitalRepository(dataProvider: HospitalDataProvider())),
+      RepositoryProvider(
+          create: (context) =>
+              RequestRepository(dataProvider: RequestDataProvider())),
+      RepositoryProvider(
+          create: (context) =>
+              SpecialTestRepository(dataProvider: SpecialTestDataProvider())),
+    ],
+    child: MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) => AuthStatusBloc(context.read<AuthRepository>())
+              ..add(CheckAuthenticationEvent())),
+        BlocProvider(
+            create: (context) => AuthBloc(context.read<AuthRepository>())),
+        BlocProvider(create: (context) => RoleBloc()),
+        BlocProvider(
+          create: (context) => hospitalBloc(
+              hospitalRepository: context.read<HospitalRepository>()),
         ),
+        BlocProvider(
+          create: (context) =>
+              ClientBloc(clientRepository: context.read<ClientRepository>()),
+        ),
+        BlocProvider(
+            create: (context) => hospitalTestsBloc(
+                hospitalRepository: context.read<HospitalRepository>())),
+        BlocProvider(
+            create: (context) => HospProfileEditBloc(
+                hospitalRepository: context.read<HospitalRepository>())),
+        BlocProvider(
+            create: (context) => hospitalListBloc(
+                hospitalRepository: context.read<HospitalRepository>())),
+        BlocProvider(
+            create: (context) => PopularHospitalBloc(
+                hospitalRepository: context.read<HospitalRepository>())),
+        BlocProvider(
+            create: (context) => requestbloc(
+                requestRepository: context.read<RequestRepository>())),
+        BlocProvider(
+            create: (context) => requestUpdatebloc(
+                requestRepository: context.read<RequestRepository>())),
+        BlocProvider(
+            create: (context) => specialTestBloc(
+                specialTestRepository: context.read<SpecialTestRepository>())),
+        BlocProvider(
+            create: (context) => CatagoryTestBloc(
+                hospitalRepository: context.read<HospitalRepository>())),
+        BlocProvider(
+            create: (context) => TestDetailBloc(
+                specialTestRepository: context.read<SpecialTestRepository>())),
+        BlocProvider(
+            create: (context) =>
+                UserBloc(userRepository: context.read<UserRepository>())),
+      ],
+      child: MaterialApp.router(
+        routeInformationParser: router().routeInformationParser,
+        routerDelegate: router().routerDelegate,
+        debugShowCheckedModeBanner: false,
+        title: "Special Tests",
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ),
+  ));
+}
+
+class SpecialTests extends StatelessWidget {
+  const SpecialTests({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AuthStatusBloc, AuthStatusState>(
+        listener: (context, state) {
+      if (state is AuthenticatedState) {
+        final userData = state.user;
+        switch (userData.role) {
+          case 'CLIENT':
+            BlocProvider.of<PopularHospitalBloc>(context)
+                .add(loadPopularHospital());
+            context.goNamed("home");
+            break;
+          default:
+            BlocProvider.of<hospitalBloc>(context).add(loadHospitalfromDb());
+            context.goNamed("hospital_profile");
+        }
+      } else {
+        context.goNamed("selectRole");
+      }
+    }, builder: (context, state) {
+      BlocProvider.of<AuthStatusBloc>(context).add(CheckAuthenticationEvent());
+      return Scaffold(
+        body: Center(
+          child: SizedBox(
+            width: 25,
+            height: 25,
+            child: CircularProgressIndicator(
+              color: AppColor.primaryColor,
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
